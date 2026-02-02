92405947b0f73a194cf95e6b889cbb7d1f133034
Zohn <42101445+ZohnHadley@users.noreply.github.com>
2026-02-02 18:46:08 -0500

    init with spring

---
+++ src/main/java/com/cal/yughistore/YughistoreApplication.java
@@ -0,0 +1,13 @@
+package com.cal.yughistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YughistoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YughistoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cal/yughistore/YughistoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cal.yughistore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YughistoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
