244e97c9ed512c7374e660b958ddc83dbd7325e5
Zohn <42101445+ZohnHadley@users.noreply.github.com>
2026-02-02 19:43:30 -0500

    added: compose.yaml, AppplicaitonConfig.class and properties in applicaiton.properties

---
+++ src/main/java/com/cal/yughistore/config/ApplicaitonConfig.java
@@ -0,0 +1,16 @@
+package com.cal.yughistore.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicaitonConfig {
+
+    @Bean
+    public ChatClient ollamaChatClient(OllamaChatModel ollamaModel) {
+        return ChatClient.create(ollamaModel);
+    }
+
+}
