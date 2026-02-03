f86eae842d76d22fecad71782cb92e6489416ac6
Zohn <42101445+ZohnHadley@users.noreply.github.com>
2026-02-02 22:17:09 -0500

    fixed misspelling of ApplicationConfig.java

--- src/main/java/com/cal/yughistore/config/ApplicationConfig.java
+++ src/main/java/com/cal/yughistore/config/ApplicationConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApplicaitonConfig {
+public class ApplicationConfig {
 
     @Bean
     public ChatClient ollamaChatClient(OllamaChatModel ollamaModel) {
