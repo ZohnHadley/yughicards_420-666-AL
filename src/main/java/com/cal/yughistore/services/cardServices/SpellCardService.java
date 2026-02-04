08a4aea138804e6ec8bcfd842c40f48465ef94bc
Zohn <42101445+ZohnHadley@users.noreply.github.com>
2026-02-04 00:32:38 -0500

    reworked backend

--- src/main/java/com/cal/yughistore/services/cardServices/SpellCardService.java
+++ src/main/java/com/cal/yughistore/services/cardServices/SpellCardService.java
@@ -3,6 +3,7 @@ package com.cal.yughistore.services.cardServices;
 import com.cal.yughistore.model.MonsterCard;
 import com.cal.yughistore.model.SpellCard;
 import com.cal.yughistore.repository.SpellCardRepository;
+import com.cal.yughistore.services.DTOs.DTOSpellCard;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +11,10 @@ import org.springframework.stereotype.Service;
 public class SpellCardService {
     private SpellCardRepository repository;
 
-    public void save(SpellCard card){
+    public void save(DTOSpellCard card){
         if (card == null){
             throw new RuntimeException("card can't be null");
         }
-        repository.save(card);
+        repository.save(card.toEntity());
     }
 }
--- src/main/java/com/cal/yughistore/services/cardServices/TrapCardService.java
+++ src/main/java/com/cal/yughistore/services/cardServices/TrapCardService.java
@@ -3,6 +3,7 @@ package com.cal.yughistore.services.cardServices;
 import com.cal.yughistore.model.SpellCard;
 import com.cal.yughistore.model.TrapCard;
 import com.cal.yughistore.repository.TrapCardRepository;
+import com.cal.yughistore.services.DTOs.DTOTrapCard;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +11,10 @@ import org.springframework.stereotype.Service;
 public class TrapCardService {
     private TrapCardRepository repository;
 
-    public void save(TrapCard card){
+    public void save(DTOTrapCard card){
         if (card == null){
             throw new RuntimeException("card can't be null");
         }
-        repository.save(card);
+        repository.save(card.toEntity());
     }
 }
