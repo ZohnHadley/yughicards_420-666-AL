dd8afdf6e211d35c25a7efa43cffd51ef3c7bb4e
Zohn <42101445+ZohnHadley@users.noreply.github.com>
2026-02-04 08:25:16 -0500

    reworked backend

--- src/main/java/com/cal/yughistore/config/ApplicationConfig.java
+++ src/main/java/com/cal/yughistore/config/ApplicationConfig.java
@@ -8,8 +8,5 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
-    @Bean
-    public ChatClient ollamaChatClient(OllamaChatModel ollamaModel) {
-        return ChatClient.create(ollamaModel);
-    }
+
 }
