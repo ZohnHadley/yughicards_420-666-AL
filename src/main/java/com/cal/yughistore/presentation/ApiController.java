f2048225e6f064e582c48b0b8f9bb4ce98d61ace
Zohn <42101445+ZohnHadley@users.noreply.github.com>
2026-02-10 01:13:26 -0500

    (WIP) api + json utils and file read

---
+++ src/main/java/com/cal/yughistore/presentation/ApiController.java
@@ -0,0 +1,14 @@
+package com.cal.yughistore.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:5173")
+public class ApiController {
+
+}
